939c7e8f27c7182c0817f696c41ef7aad4f29aa7
Akshay Buddiga <abuddiga@stanford.edu>
2016-01-23 16:41:20 -0800

    Implement circle transitions

--- sort.js
+++ sort.js
@@ -1,4 +1,3 @@
-var circleArray = [];
 
 var unpixelize = function(pixels) {
 	return Number(pixels.slice(0,-2));
@@ -7,6 +6,7 @@ var unpixelize = function(pixels) {
 var settings = {
 	width: d3.select(".sort").style("width"),
 	height: d3.select(".sort").style("height"),
+	min_radius: 20,
 	n: 10,
 };
 
@@ -14,13 +14,14 @@ settings.max_radius = unpixelize(settings.width)/(2*settings.n);
 	// unpixelize(settings.height)/(2*settings.n)
 
 console.log(settings);
+var circleArray = [];
 
 var circleGenerator = function(n) {
 	var radius;
 	var currX = 0;
 
 	var generateRand = function() {
-		return Math.floor(Math.random() * settings.max_radius);
+		return Math.floor(Math.random() * (settings.max_radius - settings.min_radius) + settings.min_radius);
 	};
 
 	for (var i = 0; i < n; i++) {
@@ -41,11 +42,11 @@ var circleGenerator = function(n) {
 circleGenerator(settings.n);
 
 //var sortDiv = d3.select(".sort");
-var circles = d3.select(".sort").selectAll("svg.player").data(circleArray);
+var circles = d3.select(".sort").selectAll("svg.player");
 
 
 var createCircles = function() {
-	circles.enter()
+	circles.data(circleArray).enter()
 		.append("svg")
 		.attr("width", function(d) {
 			return d3.select(this.parentNode).style("width");
@@ -66,5 +67,69 @@ var createCircles = function() {
 		});
 
 };
+
+var bubbleSort = function() {
+	// var newCircleArray = circleArray.slice(0);
+	// var tmp = newCircleArray[0];
+	// newCircleArray[0] = newCircleArray[1];
+	// newCircleArray[1] = tmp;
+	// console.log(circleArray);
+	// console.log(newCircleArray);
+
+	var generate = function(n, limit) {
+		var result = [];
+		for (var i =0; i < n; i++) {
+			result.push(Math.floor(Math.random() * limit));
+		}
+
+		return result;
+	}
+
+	var arrR = generate(circleArray.length, settings.max_radius);
+
+	d3.selectAll('circle').data(circleArray)
+	.transition()
+	// .attr('r', function(d, i) {
+	// 	console.log(arrR[i]);
+	// 	return arrR[i];
+	// })
+	// .attr('cy', function(d) {
+	// 	return d.attr('cy');
+	// })
+	.attr('cx', function(d, i) {
+		return generate(1, unpixelize(settings.width));
+	})
+	.duration(500);
+	// var newCircleOne = {
+	// 	radius: 20,
+	// 	currentX: 20
+	// };
+
+	// var newCircleTwo = {
+	// 	radius: 20,
+	// 	currentX: 60
+	// };
+	// circles.data([newCircleOne, newCircleTwo]).transition();
+};
+
+// var bubbleSort = function(array) {
+//   var end = circleArray.length;
+//   var swapped = true;
+//   var tmp;
+
+//   while (swapped) {
+//   	swapped = false;
+//   	for (var j = 0; j < end; j++) {
+//   		if (array[j] > array[j+1]) {
+//   			tmp = array[j+1];
+//   			array[j+1] = array[j];
+//   			array[j] = tmp;
+//   			swapped = true;
+//   		}
+//   	}
+//   	end--;
+//   }
+//   return array;
+// };
 
 createCircles();
